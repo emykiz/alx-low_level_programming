dcf67aff0a865a436bd6db59208e3938bf69fabd
emykiz <you@example.comgit config --global user.email you@example.com>
2022-09-08 12:29:56 -0700

    gh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - main block
  * Return: 0
- * */
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
