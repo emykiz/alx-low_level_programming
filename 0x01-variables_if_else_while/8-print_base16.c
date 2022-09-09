4d73d00f35c3d4a1571939f2c9eca408fe2cf741
emykiz <you@example.comgit config --global user.email you@example.com>
2022-09-09 14:18:44 -0700

    ghss

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,17 +3,18 @@
 #include <time.h>
 /**
  * main - Prints a hexadecimal string
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char c;
+int c;
+char low;
 for (c = '0'; c <= '9'; c++)
 {
 putchar(c);
-for (c = 'a'; c <= 'f'; c++)
+for (low = 'a'; low <= 'f'; low++)
 {
-putchar(c);
-}
+putchar(low);
 putchar('\n');
-return (0);}
+return (0);
+}
