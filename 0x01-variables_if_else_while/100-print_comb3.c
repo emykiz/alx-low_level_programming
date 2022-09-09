82981385275371ec99e1af6d324935173fa5fa5e
emykiz <you@example.comgit config --global user.email you@example.com>
2022-09-09 15:11:10 -0700

    ghn

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the numbers from 01 to 89
+ * Numbers must be separated by , followed by a space
+ * Numbers should be printed in ascending order with 2 digits
+ * You can only usethe putchar function
+ * Return:0
+ */
+int main(void)
+{
+int n1 = 48;
+int n2;
+int com= 44;
+while (n1 <=57)
+{
+putchar(n1);
+putchar(n2);
+if (n1 != 56 || n2 != 57)
+{
+putchar(com);
+putchar(32);
+}
+n2 += 1;
+}
+putchar('\n');
+return (0);
+}
