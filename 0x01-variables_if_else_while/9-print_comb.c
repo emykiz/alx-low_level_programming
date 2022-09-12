55947b8e9a53bc4acae54bebd686da13ec85ae5a
emykiz <you@example.comgit config --global user.email you@example.com>
2022-09-12 05:46:20 -0700

    lk

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,7 +4,7 @@
  *  main - main block
  *  Description: prints all single digit numbers of base 10
  *  starting from 0, followed by a new line.
- *  Return: 0 
+ *  Return: 0
  */
 int main(void)
 {
