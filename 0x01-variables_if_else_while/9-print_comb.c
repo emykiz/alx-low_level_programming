a4d96129c1f7c1e1c48846d53dd09a6d9bda9244
emykiz <you@example.comgit config --global user.email you@example.com>
2022-09-12 05:40:05 -0700

    lk

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,7 +10,7 @@ int main(void)
 {
 int c = 0;
 while (c < 10);
-}
+{
 putchar(48 + c);
 if (c != '9')
 {
@@ -18,7 +18,7 @@ putchar(',');
 putchar(' ');
 }
 c++;
-{
+}
 putchar('\n');
 return (0);
 }
