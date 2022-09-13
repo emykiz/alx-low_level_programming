c3a3dc69781ff0671a30d7c532761b9396ec3af2
emykiz <you@example.comgit config --global user.email you@example.com>
2022-09-13 14:36:15 -0700

    hj

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++
+}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+my Read me
