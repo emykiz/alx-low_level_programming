09f5d210739010648e9ad568f72c3c6dc2016a3f
emykiz <you@example.comgit config --global user.email you@example.com>
2022-09-13 21:46:03 -0700

    kl

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,14 @@
  */
 int main(void)
 {
-char text[10] = "_putchar";
-int i = 0;
-for (i = 0; i < 8; i++)
-{
-_putchar(text[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
