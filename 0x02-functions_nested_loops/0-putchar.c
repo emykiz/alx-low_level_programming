40e5239e1856e65fa3c2c54883b6d14de6b7cfd3
emykiz <you@example.comgit config --global user.email you@example.com>
2022-09-13 21:51:12 -0700

    jk

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
+ *  main -  Entry point
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
