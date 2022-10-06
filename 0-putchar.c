d8b4c797ba1cc4b01289456a285a825854661eba
emykiz <you@example.comgit config --global user.email you@example.com>
2022-10-06 12:55:44 -0700

    ghg

---
+++ 0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char text[10] = "_putchar";
+int i = 0;
+for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+return (0);
+}
